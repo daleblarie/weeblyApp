8482c967f42c2cb95763ee4b1a28ecb750f7d7b2
Dale Larie <daleblarie@gmail.com>
2018-02-02 11:17:45 -0500

    attempt at search box

--- search.js
+++ search.js
@@ -5,6 +5,7 @@
 // Use example.com as if it was a search endpoint, it doesn’t actually need to fetch results.
 // Develop a solution using Javascript/HTML and deliver your answer using a jsfiddle link.
 // The only library allowed is stock jQuery, not any of its extensions.
+
 function debounce(func, wait) {
   let timeout;
   return function returnFunc(...args) {
@@ -13,16 +14,24 @@ function debounce(func, wait) {
   };
 }
 function searchSuccessHandler(data) {
-  console.log(data);
+  const [dataList, length] = [data, data.length];
+  let nums;
+  for (let i = 0; i < 3; i += 1) {
+    nums = Math.floor(Math.random() * length);
+    const returnList = [dataList[nums]];
+    $('.returndata').append('<p>' + returnList + '</p>');
+
+    console.log(returnList);
+  }
   // pretend it gave good stuff
   // create array of data and display it
   // make an array thats like 10 items long randomly select 3 of them and inject them into the page
   // clear the list on the page before displaying
 }
-
+// const Data = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10'];
 const sendSearch = function sendSearch(event) {
   const currentQuery = event.target.value;
-  $.ajax(`example.com?query=${currentQuery}`, { success: searchSuccessHandler });
+  $.ajax(`https://www.example.com/?query=${currentQuery}`, { success: searchSuccessHandler });
 };
 const debouncedSendSearch = debounce(sendSearch, 1000);
 
